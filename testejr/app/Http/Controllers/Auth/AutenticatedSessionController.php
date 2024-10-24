8f8d0ff50ac3438b78a81b09aee1691224717287
victorjoao099 <joaovcsantos2004@gmail.com>
2024-10-24 11:16:18 -0300

    Feat: Adição dos requests e a conclusão de alguns controllers

--- testejr/app/Http/Controllers/Auth/AutenticatedSessionController.php
+++ testejr/app/Http/Controllers/Auth/AutenticatedSessionController.php
@@ -1,11 +1,29 @@
 <?php
 
-namespace App\Http\Controllers\.\Auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class AutenticatedSessionController extends Controller
 {
-    //
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    // Lidando com uma requisição recebida
+
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return Redirect()->intended(route('newsIndex', absolute: false));
+    }
 }
--- testejr/app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ testejr/app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -1,11 +1,32 @@
 <?php
 
-namespace App\Http\Controllers\.\Auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class ConfirmablePasswordController extends Controller
 {
-    //
+    public function show(): View
+    {
+        return view('auth.confirm-password');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        if (! Auth::guard('web')->validate([
+            'email' => $request->user()->email,
+            'senha' => $request->password
+        ])) {
+            throw ValidationException::withMessages([
+                'senha' => __('auth.password'),
+            ]);
+        }
+
+        return redirect()-> intended(route('dashboard', absolute: false));
+    }
 }
