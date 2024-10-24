0fb4168435bce588903347a445ff1c1106dc9b8f
victorjoao099 <joaovcsantos2004@gmail.com>
2024-10-24 11:44:14 -0300

    Feat: Adição de código de mais três controllers

--- testejr/app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ testejr/app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\.\Auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class EmailVerificationPromptController extends Controller
 {
-    //
+    public function __invoke(Request $request): RedirectResponse|View
+    {
+        return $request()->user()->hasVerifyEmail()
+                    ? redirect()->intended(route('dashboard', absolute: false))
+                    : view('auth.verify-email');
+    }
 }
--- testejr/app/Http/Controllers/Auth/NewPasswordController.php
+++ testejr/app/Http/Controllers/Auth/NewPasswordController.php
@@ -1,11 +1,46 @@
 <?php
 
-namespace App\Http\Controllers\.\Auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class NewPasswordController extends Controller
 {
-    //
+    public function create(Request $request): View
+    {
+        return view('auth.reset-password', ['request' => $request]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with('status', __($status))
+                    : back()->withInput($request->only('email'))
+                            ->withErrors(['email' => __($status)]);
+    }
 }
